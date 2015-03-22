1376a08f9d11282fec14c95cfc3499200ded5aa7
Lewis Needham <lewisn93@gmail.com>
2015-03-22 19:09:41 +0000

    Service names and bug fixes
    Dummy service names replaced with real data submitted by user.
    
    Bugs fixed:
    Refreshing page when logged in no longer returns a blank page. Instead
    user is directed to overview page.
    Closing app on Android (when packaged with PhoneGap Build) no longer
    logs user out.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 var PARSE_APP = "hDsnLQIJnntsfTgxiYXueyWwZ1kMFUdr0UUjyfIU";
 var PARSE_JS = "fPQ2l1NVewvTdAYZihOI9y9GrPYBx7sZE3etfclZ";
 
+
 //Verifies keys
 Parse.initialize(PARSE_APP, PARSE_JS);
 
@@ -10,8 +11,48 @@ angular.module('subApp', [])
 	.run(['$rootScope', function ($scope) {
 
 		//Sets first page to login and defines user
-		$scope.state = 'Log in';
 		$scope.currentUser = Parse.User.current();
+		
+		//Checks if user is logged in and loads relevant page on refresh
+		var currentUser = Parse.User.current();
+		if (currentUser) {
+			$scope.state = 'Overview';
+		} else {
+			$scope.state = 'Log in';
+		}
+
+		//Defining request to 'Services' table in db to be usable in all $scopes
+		var Services = Parse.Object.extend("Services");
+		var services = new Services();
+		//Function to return service names created by user - executed on Log in
+		var returnServices = function () {
+			var query = new Parse.Query(Services);
+			query.exists("serviceName");
+			query.find({
+				success: function (results) {
+					//clears current list
+					(function ($) {
+						$("#service-table li").remove();
+					})(jQuery);
+
+					//retrives new up to date list
+					for (var i = 0; i < results.length; i++) {
+						var object = results[i];
+						(function ($) {
+							$('#service-table').append('<li class="list-group-item"><a href="#">' + object.get('serviceName') + '</a></li>');
+						})(jQuery);
+						//console.log(object.get('serviceName'));
+					}
+				},
+				error: function (error) {
+					alert("Error: " + error.code + " " + error.message);
+				}
+			});
+		};
+
+		returnServices(); //Function is called here so after login it runs again every page refresh
+
+		
 
 		//Logs user in
 		$scope.logIn = function (form) {
@@ -20,30 +61,34 @@ angular.module('subApp', [])
 					$scope.currentUser = user;
 					$scope.$apply();
 					$scope.state = 'Overview';
+					returnServices();
+					var logInState = true;
 				},
 				error: function (user, error) {
-					alert("Unable to log in: " + error.message);
+					alert("Log in failed: " + error.message);
 				}
 			});
 		};
+
 
 		//Lets user register and saves account in Parse db
 		$scope.signUp = function (form) {
 			var user = new Parse.User();
-			user.set("email", form.email);
 			user.set("username", form.username);
 			user.set("password", form.password);
-
+			user.set("email", form.email);
 			user.signUp(null, {
 				success: function (user) {
 					$scope.currentUser = user;
 					$scope.$apply();
+					$scope.state = 'Overview';
 				},
 				error: function (user, error) {
-					alert("Unable to sign up:  " + error.message);
+					alert("Sign up failed:  " + error.message);
 				}
 			});
 		};
+
 
 		//Logs current user out and goes back to login page
 		$scope.logOut = function (form) {
@@ -52,10 +97,35 @@ angular.module('subApp', [])
 			$scope.state = 'Log in';
 		};
 
+
+
+		//Function to return most the single recent service added by user - executed upon adding new service
+		var returnNewService = function () {
+			var query = new Parse.Query(Services);
+			query.exists("serviceName");
+			query.limit(1);
+			query.first({
+				success: function (results) {
+					for (var i = 0; i < results.length; i++) {
+						var object = results[i];
+						(function ($) {
+							$('#service-table').append('<li class="list-group-item"><a href="#">' + object.get('serviceName') + '</a></li>');
+						})(jQuery);
+						console.log('Services returned: ' + object.get('serviceName'));
+					}
+				},
+				error: function (error) {
+					alert("Error: " + error.code + " " + error.message);
+				}
+			});
+		};
+
+
+
 		//Adds new subscription details to Parse db
 		$scope.newSub = function (form) {
-			var Services = Parse.Object.extend("Services");
-			var services = new Services();
+			//var Services = Parse.Object.extend("Services");
+			//var services = new Services();
 			services.set("serviceName", form.serviceName);
 			services.set("costMonthly", form.costMonthly);
 			services.set("renewalPeriodMonthly", form.renewalPeriodMonthly);
@@ -64,20 +134,27 @@ angular.module('subApp', [])
 			services.setACL(new Parse.ACL(Parse.User.current()));
 			services.save(null, {
 				success: function (services) {
-					//alert('New service added: ' + services.serviceName);
+					console.log('New service added: ' + services.get("serviceName"));
+					returnServices();
 				},
 				error: function (services, error) {
 					alert('Failed to create new object, with error code: ' + error.message);
 				}
 			});
 			$scope.state = 'Services';
-
-			
 		};
 
 
+/*  I.1 - SWITCHES HEADER TO SMALLER VERSION - DON'T USE UNTIL MOBILE ONLY
+		$(".minimiseHeader").click(function () {
+			$(".jumbo-header").fadeToggle(10);
+			$(".small-header").show(10);
+		});
 
-
-
+		$(".maximiseHeader").click(function () {
+			$(".jumbo-header").fadeToggle(10);
+			$(".small-header").hide(10);
+		});
+*/
 
 	}]);

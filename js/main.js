5c3b5115ee8efcb3a08842f6c1cd17b155b30736
Lewis Needham <lewisn93@gmail.com>
2015-05-08 21:54:52 +0100

    Calc improvements
    User can now switch between monthly and yearly calculated cost of their
    combined subscription fees.
    
    More code comments added.

--- js/main.js
+++ js/main.js
@@ -10,12 +10,13 @@ Parse.initialize(PARSE_APP, PARSE_JS);
 angular.module('subApp', [])
 	.run(['$rootScope', function ($scope) {
 
+		
+		
 		//Sets first page to login and defines user
 		$scope.currentUser = Parse.User.current();
-
-		//Checks if user is logged in and loads relevant page on refresh
 		var currentUser = Parse.User.current();
 
+		//Function to checks if user is logged in and loads relevant page on refresh
 		var pageCheck = function () {
 			if (currentUser) {
 				$scope.state = 'Overview';
@@ -25,8 +26,7 @@ angular.module('subApp', [])
 				console.log('No user logged in');
 			}
 		};
-
-		pageCheck();
+		pageCheck(); //execute
 
 
 
@@ -37,6 +37,7 @@ angular.module('subApp', [])
 		//Function to return service names created by user - executed on Log in
 		var returnServices = function () {
 			var query = new Parse.Query(Services);
+			//Queries any existing objects in serviceName column
 			query.exists("serviceName");
 			query.find({
 				success: function (results) {
@@ -44,12 +45,14 @@ angular.module('subApp', [])
 					(function ($) {
 						$("#service-table li").remove();
 					})(jQuery);
-
 					//retrives new up to date list
 					for (var i = 0; i < results.length; i++) {
 						var object = results[i];
+						//Inserts each retrived serviceName as a list item into the HTML
+						var fetchServiceName = object.get('serviceName');
+						
 						(function ($) {
-							$('#service-table').append('<li class="list-group-item"><a href="#">' + object.get('serviceName') + '</a></li>');
+							$('#service-table').append('<li class="list-group-item"><a href="#" id="listItemService">' + fetchServiceName + '</a></li>');
 						})(jQuery);
 						//console.log(object.get('serviceName'));
 					}
@@ -60,10 +63,10 @@ angular.module('subApp', [])
 			});
 		};
 
-
+		//Function to return total cost of subscription fees
 		var returnTotalCost = function () {
-			var costMonthlyArray = [];
 			var query = new Parse.Query(Services);
+			//Queries any existing objects in costMonthly column
 			query.exists("costMonthly");
 			query.find({
 				success: function (results) {
@@ -75,13 +78,51 @@ angular.module('subApp', [])
 
 					//Checks if total fees exists and alters page content accordingly
 					if (sum == 0) {
-						(function ($) {
-							$('#total-cost').empty().append('<p class="large">You currently have no subscriptions to monitor</br></br>Try adding some</p>');
-						})(jQuery);
+
+						//Hides elements if subscription fees don't exist				
+						$(".hiddenUntilValid").hide();
+
 					} else {
 						(function ($) {
-							$('#total-cost').empty().append('<p class="large">Your <strong>TOTAL</strong> subscription fees cost </p>' + '<p class="huge">£' + (sum.toFixed(2)) + '</p><p class="large"> per month</p>');
+							$('#totalCostValue').empty().append('£' + (sum.toFixed(2)));
 						})(jQuery);
+
+						//Hides placeholder class
+						$(".shownUntilInvalid").hide();
+
+						//Restores elements if they were hidden
+						$(".hiddenUntilValid").show();
+
+
+						//Sets default button appearance
+						document.getElementById("totalMonthlyCost").className =
+							document.getElementById("totalMonthlyCost").className.replace(/(?:^|\s)btn-default(?!\S)/g, '')
+
+						//When 'Yearly' button is clicked
+						$('#totalYearlyCost').click(function () {
+							//Removes inactive class from yearly button
+							document.getElementById("totalYearlyCost").className =
+								document.getElementById("totalYearlyCost").className.replace(/(?:^|\s)btn-default(?!\S)/g, '')
+								//And adds it to monthly button
+							document.getElementById('totalMonthlyCost').className += ' btn-default';
+							//Multiplies monthly total by 12
+							$('#totalCostValue').empty().append('£' + (sum * 12).toFixed(2));
+						})
+
+						//When 'Monthly' button is clicked
+						$('#totalMonthlyCost').click(function () {
+							//Removes inactive class from monthly button
+							document.getElementById("totalMonthlyCost").className =
+								document.getElementById("totalMonthlyCost").className.replace(/(?:^|\s)btn-default(?!\S)/g, '')
+								//And adds it to yearly button
+							document.getElementById('totalYearlyCost').className += ' btn-default';
+							//Returns monthly total
+							$('#totalCostValue').empty().append('£' + (sum.toFixed(2)));
+						})
+
+
+
+
 					}
 
 
@@ -93,15 +134,14 @@ angular.module('subApp', [])
 		};
 
 
-		var returnUpcomingRenewals = function () {
 
 
-		};
+		var returnUpcomingRenewals = function () {};
 
 
 
 
-		returnServices(); //Functions are called here so after login it runs again every page refresh
+		returnServices(); //Functions are called here so after login they run again every page refresh
 		returnTotalCost();
 
 		//Logs user in
